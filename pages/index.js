d638252f4c408fda22d44e9c98b7697ba614c408
romain <romain.wojciechowski.mode83@gmail.com>
2023-02-07 11:58:56 +0100

    b2 branching

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }) {
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Neeeeeeeeeeeeeext.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
